3be24aa69caa7a76f24503a4152910d8e079f7f0
UgurDemirelll <200103011007@gaziantep.edu.tr>
2022-08-04 00:20:31 +0300

    Tasarımsal yenilikler veri tabanı işlemleri

--- app/src/main/java/com/ugurd/guideapp2/EditTopicFragment.kt
+++ app/src/main/java/com/ugurd/guideapp2/EditTopicFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_display_select.view.*
 import kotlinx.android.synthetic.main.fragment_edit_topic.view.*
@@ -24,15 +25,17 @@ class EditTopicFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_edit_topic, container, false)
-
+        topics.add("Konu Seç")
+        topics.add("başka konu")
+        issues.add("ayraçlar listelenecek")
+        
 
 try {
     activity?.let {
-        topics.add("Konu Seç")
-        issues.add("ayraçlar listelenecek")
-        dataAdapter = ArrayAdapter(it.applicationContext,android.R.layout.simple_list_item_activated_1,android.R.id.text1,topics)
-        dataAdapter2 = ArrayAdapter(it.applicationContext,android.R.layout.simple_list_item_activated_1,android.R.id.text1,issues)
-        view.spinnerDisplay.adapter = dataAdapter
+
+        dataAdapter = ArrayAdapter(it,android.R.layout.simple_list_item_activated_1,android.R.id.text1,topics)
+        dataAdapter2 = ArrayAdapter(it,android.R.layout.simple_list_item_activated_1,android.R.id.text1,issues)
+        view.spinnerEditTopic.adapter = dataAdapter
         println("try çalışıyor")
         view.spinnerEditIssue.adapter = dataAdapter2
     }
