f6d003965b9bc0c403ab33f8ea6500d79e303a61
UgurDemirelll <200103011007@gaziantep.edu.tr>
2022-08-05 21:30:33 +0300

    recyclerview ayarları

---
+++ app/src/main/java/com/ugurd/guideapp2/DisplayRecyclerAdapter.kt
@@ -0,0 +1,29 @@
+package com.ugurd.guideapp2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.recycler_row_display.view.*
+
+class DisplayRecyclerAdapter(val issueDisplayList : ArrayList<String>, val idList : ArrayList<Int>) : RecyclerView.Adapter<DisplayRecyclerAdapter.TopicHolder>() {
+    class TopicHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopicHolder {
+
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.recycler_row_display,parent,false)
+        return  TopicHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TopicHolder, position: Int) {
+        holder.itemView.text_recycler_display.text = issueDisplayList[position]
+    }
+
+    override fun getItemCount(): Int {
+        return issueDisplayList.size
+
+    }
+}
